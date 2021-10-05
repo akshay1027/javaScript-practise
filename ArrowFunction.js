5f4521e1b1c5ee67c1a73e523804d8dff27eecb2
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-10-05 23:58:33 +0530

    Create ArrowFunction.js

---
+++ ArrowFunction.js
@@ -0,0 +1,24 @@
+// using function
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+// using arrow function: 
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
